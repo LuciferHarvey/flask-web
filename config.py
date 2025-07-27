2c11fccff12e4d9d6c47ba4a2f9b424a06248590
Lucifer Harvey <ngocang5@outlook.com>
2025-07-28 02:03:06 +0700

    Stable

--- config.py
+++ config.py
@@ -5,5 +5,12 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-    MONGODB_URI = os.environ.get('MONGODB_URI') or 'mongodb://localhost:27017/flask_blog'
     FLASK_ENV = os.environ.get('FLASK_ENV') or 'development'
+    MONGODB_USER = os.environ.get('MONGO_INITDB_ROOT_USERNAME')
+    MONGODB_PASSWORD = os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongo')
+    MONGODB_PORT = os.environ.get('MONGODB_PORT', '27017')
+    MONGODB_DB = os.environ.get('MONGO_INITDB_DATABASE', 'flask_blog')
+
+    # Build URI với authSource=admin để xác thực user root
+    MONGODB_URI = os.environ.get('MONGODB_URI')
